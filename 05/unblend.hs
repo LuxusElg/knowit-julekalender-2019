1cdbe7e881f6d9bb6f52d529661b758af8765277
luxuselg <cbentzrud@gmail.com>
2019-12-05 16:07:26 +0100

    wip haskell stuff

---
+++ 05/unblend.hs
@@ -0,0 +1,12 @@
+module Main
+    where
+
+split string =
+    let half = length string / 2
+    filter string
+
+half string =
+
+
+-- oepHlpslainttnotePmseormoTtlst
+main = putStrLn (split "Hello World")
