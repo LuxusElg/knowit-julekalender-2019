459aa2b420f0407574a2d35aaf86779f456a0e70
LuxusElg <cbentzrud@gmail.com>
2019-12-05 16:51:53 +0100

    05 solved!

--- 05/unblend.hs
+++ 05/unblend.hs
@@ -1,12 +1,21 @@
 module Main
     where
 
-split string =
-    let half = length string / 2
-    filter string
+fronttoback string =
+    let half = div (length string) 2 in
+        drop half string ++ take half string
 
-half string =
+pairswap [] = []
+pairswap string =
+    let pair = take 2 string in
+        (tail pair ++ [head pair]) ++ pairswap (drop (length pair) string)
 
+tripleswap [] = []
+tripleswap string =
+    let front = take 3 string
+        back = drop (length string - 3) string
+        middle = take (length string - 6) (drop 3 string)
+        in back ++ tripleswap middle ++ front
 
--- oepHlpslainttnotePmseormoTtlst
-main = putStrLn (split "Hello World")
+        -- test string= oepHlpslainttnotePmseormoTtlst
+main = putStrLn (fronttoback (pairswap (tripleswap "tMlsioaplnKlflgiruKanliaebeLlkslikkpnerikTasatamkDpsdakeraBeIdaegptnuaKtmteorpuTaTtbtsesOHXxonibmksekaaoaKtrssegnveinRedlkkkroeekVtkekymmlooLnanoKtlstoepHrpeutdynfSneloietbol")))
